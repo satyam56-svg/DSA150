108f20498b7cc7cad310f561052e75b7a5a2ff2e
satyam56-svg <satyamkumar2351056@gmail.com>
2026-01-31 23:38:23 +0530

    LB day 8

---
+++ LoveBabbarSheet/RanSom.java
@@ -0,0 +1,25 @@
+package LoveBabbarSheet;
+
+import java.util.HashMap;
+
+public class RanSom {
+    public static boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> mag = new HashMap<>();
+        // Count magazine
+        for (char ch : magazine.toCharArray()) {
+            mag.put(ch, mag.getOrDefault(ch, 0) + 1);
+        }
+        // Check ransomNote
+        for (char ch : ransomNote.toCharArray()) {
+            if (!mag.containsKey(ch) || mag.get(ch) == 0) {
+                return false;
+            }
+            mag.put(ch, mag.get(ch) - 1);
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(canConstruct("aaa","aab"));
+    }
+}
