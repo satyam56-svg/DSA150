cb5f60d572f93a5e3ba09f6457730f184e3fda5c
satyam56-svg <satyamkumar2351056@gmail.com>
2026-02-01 21:35:46 +0530

    LB day 9

---
+++ LoveBabbarSheet/median2.java
@@ -0,0 +1,22 @@
+// package LoveBabbarSheet;
+
+// import java.util.Arrays;
+
+// public class median2 {
+//     public static double findMedian(int[] arr) {
+//         // Code here.
+//         int n=arr.length;
+//         Arrays.sort(arr);
+        
+//         if(n%2!=0) {
+//             return arr[n/2];
+//         } else {
+//             return (arr[n/2-1]+arr[(n/2)])/2.0;
+//         }
+//     }
+
+//     public static void main(String[] args) {
+//         int arr[] = {5, 3, 1, 4, 2};
+//         System.out.println(findMedian(arr));  // Outputs: 3.0
+//     }
+// }
