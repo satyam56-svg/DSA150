5c9ff364cfc1940dcc6d9f70b7596160ccfd7458
satyam56-svg <satyamkumar2351056@gmail.com>
2026-03-09 20:30:30 +0530

    LB day 43

---
+++ LoveBabbarSheet/largest_swap.java
@@ -0,0 +1,26 @@
+public class largest_swap {
+    public static String largestSwap(String s) {
+        char[] arr = s.toCharArray();
+        int[] last = new int[10];
+
+        for(int i = 0; i < arr.length; i++)
+            last[arr[i] - '0'] = i;
+
+        for(int i = 0; i < arr.length; i++) {
+            for(int d = 9; d > arr[i] - '0'; d--) {
+                if(last[d] > i) {
+                    char t = arr[i];
+                    arr[i] = arr[last[d]];
+                    arr[last[d]] = t;
+                    return new String(arr);
+                }
+            }
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+        String s = "2736";
+        System.out.println(largestSwap(s));
+    }
+}
--- LoveBabbarSheet/rearrange_arr.java
+++ LoveBabbarSheet/rearrange_arr.java
@@ -1,36 +1,32 @@
-// import java.util.Arrays;
+import java.util.ArrayList;
 
-// public class rearrange_arr {
-//     public static void rearrange(int arr[]) {
-//         Arrays.sort(arr);  // [1,2,3,4,5,6]
-        
-//         int n = arr.length;
-//         int[] result = new int[n];
-        
-//         // Two pointers approach
-//         int left = 0, right = n-1;
-//         int idx = 0;
-        
-//         while(left <= right) {
-//             // Max element at even indices
-//             if(idx % 2 == 0) {
-//                 result[idx++] = arr[right--];
-//             }
-//             // Min element at odd indices  
-//             else {
-//                 result[idx++] = arr[left++];
-//             }
-//         }
-        
-//         // Copy back to original array
-//         for(int i = 0; i < n; i++) {
-//             arr[i] = result[i];
-//         }
-//     }
+public class rearrange_arr {
 
-//     public static void main(String[] args) {
-//         int[] arr = {1, 2, 3, 4, 5, 6};
-//         rearrange(arr);
-//         System.out.println(Arrays.toString(arr)); // Output: [6, 1, 5, 2, 4, 3]
-//     }
-// }
+    public static void Rearrange(int a[], int n) {
+        ArrayList<Integer>neg=new ArrayList<>();
+        ArrayList<Integer>pos=new ArrayList<>();
+        for(int num:a) {
+            if(num<0) {
+                neg.add(num);
+            } else {
+                pos.add(num);
+            }
+        }
+        
+        for(int i=0; i<neg.size(); i++) {
+            a[i]=neg.get(i);
+        }
+        
+        for(int i=0; i<pos.size(); i++) {
+            a[i+neg.size()]=pos.get(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, -2, 3, -4, 5, -6};
+        Rearrange(a, a.length);
+        for(int num : a) {
+            System.out.print(num + " ");
+        }
+    }
+}
