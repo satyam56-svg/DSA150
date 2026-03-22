84abf84326d487ede687b648e34efb0e9058ec82
satyam56-svg <satyamkumar2351056@gmail.com>
2026-03-22 18:56:33 +0530

    LB day 55

---
+++ LoveBabbarSheet/Searching/shipWithinDays.java
@@ -0,0 +1,54 @@
+// public class shipWithinDays {
+//     public static int shipWithinDays(int[] weights, int days) {
+//         int n = weights.length;
+
+//         int low = 0;
+//         int high = 0;
+
+//         // find max and sum
+//         for (int i = 0; i < n; i++) {
+//             if (weights[i] > low) {
+//                 low = weights[i];   // max element
+//             }
+//             high += weights[i];     // total sum
+//         }
+
+//         int ans = high;
+
+//         while (low <= high) {
+//             int mid = low + (high - low) / 2;
+
+//             if (isPossible(weights, n, days, mid)) {
+//                 ans = mid;
+//                 high = mid - 1;   // try smaller capacity
+//             } else {
+//                 low = mid + 1;    // increase capacity
+//             }
+//         }
+
+//         return ans;
+//     }
+
+//     public static boolean isPossible(int[] weights, int n, int days, int capacity) {
+//         int daysUsed = 1;
+//         int currentLoad = 0;
+
+//         for (int i = 0; i < n; i++) {
+//             if (currentLoad + weights[i] <= capacity) {
+//                 currentLoad += weights[i];
+//             } else {
+//                 daysUsed++;
+//                 currentLoad = weights[i];
+//             }
+//         }
+
+//         return daysUsed <= days;
+//     }
+
+//     public static void main(String[] args) {
+//         int[] weights = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//         int days = 5;
+//         System.out.println(shipWithinDays(weights, days)); // Output: 15
+//     }
+// }
+
